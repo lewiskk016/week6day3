a1351d6ce0908b3887306270ef3e7b596a9522cb
jliangrpb <jeremy.liang612@gmail.com>
2023-04-12 14:19:59 -0400

    Finished users routes

--- Project_JK/config/routes.rb
+++ Project_JK/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users 
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+  get '/users', to: 'users#new' 
+  get '/users/id/edit', to: 'users#edit'
+  get    '/users/:id', to: 'users#show'
+  patch  '/users/:id', to: 'users#update'
+  put   '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 end
