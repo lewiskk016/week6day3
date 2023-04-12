20f157760e8a10f21e12ac9f5dff342714cc98ab
lewiskk016 <lewiskk016@gmail.com>
2023-04-12 17:23:20 -0400

    almost finished

--- Project_JK/app/controllers/users_controller.rb
+++ Project_JK/app/controllers/users_controller.rb
@@ -4,11 +4,20 @@ class UsersController < ApplicationController
         render plain: "I'm in the index action!"
     end
 
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
     def create
-        render json: params
+
+        @user = User.new(user_params)
+        if  @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
-    def show 
-        render json: params 
+    def show
+        render json: params
     end
 end
