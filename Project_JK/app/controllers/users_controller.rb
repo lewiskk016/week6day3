f3fd444d487009f74ff31c1dc8564d1c83da8349
lewiskk016 <lewiskk016@gmail.com>
2023-04-13 09:42:31 -0400

    finish first routes

--- Project_JK/app/controllers/users_controller.rb
+++ Project_JK/app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
 
     def user_params
         params.require(:user).permit(:name, :email)
     end
+
     def create
 
         @user = User.new(user_params)
@@ -18,6 +20,22 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params
+            @user = User.find(params[:id])
+            render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url
     end
 end
